2ff345d583c43aab67cb9395f2b3c237dcdfe971
Ahmed Reza <ahmedreza80@gmail.com>
2019-08-06 23:29:24 +0530

    dfhjd

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Nearnew
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.load_path += Dir[root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
